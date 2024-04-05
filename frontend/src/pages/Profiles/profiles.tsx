c782076592b9a83da9e0201016861b013033b3f0
unary-code <zacharykatz1618@gmail.com>
2024-04-04 20:47:07 -0500

    Added position styling for the profile page

--- frontend/src/pages/Profiles/profiles.tsx
+++ frontend/src/pages/Profiles/profiles.tsx
@@ -3,25 +3,80 @@ import styles from "./profiles.module.css";
 // import { Link } from "react-router-dom";
 
 function ProfilePage(): React.ReactNode {
+    const attributesList = [{name: "Age", value: 25},
+    {name: "Gender", value: "Male"},
+    {name: "Phone Number", value: "1-888-888-8888"},
+    {name: "Email", value: "explorer@gmail.com"}];
+
+    const objKeysToDisplayedNames = {"personalInfo": "Personal Information",
+    "legalFirstName":"Legal First Name", "gettingStarted": "Getting Started", "troubleshooting": "Troubleshooting"}
+    const profileObj = {personalInfo: {legalFirstName: "Justin", legalLastName: "Baldoni", dob: "01/01/2017", emergencyContact: "Boots"}, gettingStarted: {}, troubleshooting: {}};
+
     return <>
         <div className={styles['profile-page']}>
             <div className={styles['hero']}>
-                <section>
-                    <img src="/username-profile-pic.png"/>
-                    <h3>Dora Marquez</h3>
-                    <p className={styles["subtitle"]}>patient</p>
-                </section>
-                <section>
-                    <img src="/history.png"/>
-                    <h3>view history</h3>
-                </section>
-                <hr></hr>
-                <section>
-                    <p className={styles["subtitle"]}>Age</p>
-                    <p className={styles["subtitle"]}>Gender</p>
-                    <p className={styles["subtitle"]}>Phone Number</p>
-                    <p className={styles["subtitle"]}>Email</p>
-                </section>
+                <div className={styles['profile-summary-container']}>
+
+                    <section className={styles['profile-row-with-avatar']}>
+                        <div className={styles['profile-row-inner']}>
+                            <img src="images/profile-pic-justin-baldoni.jpeg" className={styles["profile-avatar"] + " " + styles["first-ele-in-row"]} />
+                            <div className={styles['profile-basic-description-container']}>
+                                <h3>Justin Baldoni</h3>
+                                <p className={styles["subtitle"]}>patient</p>
+                            </div>
+
+                            <div className={styles['history-container']}>
+                                <div className={styles['history-container-inner']}>
+                                <img src="icons/activity-history.png" className={styles["history-icon"]}/>
+                                <div className={styles["bottom-aligned"]}>
+                                    <h3>view history</h3>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    
+                    <hr></hr>
+                    <section className={styles['profile-row-after-hr'] + " " + styles['profile-last-row']}>
+                        <div className={styles['profile-row-inner']}>
+                            <div className={styles['profile-attributes']}>
+                                {attributesList.map((attributeObj, ind) => (<div className={styles["profile-attribute"]}>
+                                    <p className={styles["subtitle"]}>{attributeObj.name}</p>
+                                    <p className={styles["subtitle"]}>{attributeObj.value}</p>
+                                </div>))}
+                            
+
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+
+                <div className={styles['profile-tabs-and-tables']}>
+                    <div className={styles['profile-tabs']}>
+                        <ul className={styles['no-bullet-points']}>
+                            {Object.entries(profileObj).map(([key, value]) => <li>{(key in objKeysToDisplayedNames) ? objKeysToDisplayedNames[key] : key}</li>)}
+
+                        </ul>
+                    </div>
+                    <div className={styles['profile-tables']}>
+                    <div className={styles['profile-tables-inner']}>
+                        <div className={styles['profile-tables-header-row']}>
+                            <p>Personal Information</p>
+                        </div>
+                        <hr></hr>
+
+                        <div className={styles['profile-table-container']}>
+                            <table>
+                                {Object.entries(profileObj["personalInfo"]).map(([key, value]) => <tr>
+                                    <td>{(key in objKeysToDisplayedNames) ? objKeysToDisplayedNames[key] : key}</td>
+                                    <td>{value}</td>
+                                    </tr>)}
+                            </table>
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>;
